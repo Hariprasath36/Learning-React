52f5501fefb9715e94f80dca8a6c012f4efd8dfd
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-12-26 09:29:45 +0530

    running 2:04:00

--- react-tutorial/src/components/Grage.js
+++ react-tutorial/src/components/Grage.js
@@ -1,9 +1,26 @@
 import Car from './Car';
 
 function Garage() {
+
+    const CarList =[
+        { brand: 'Ford', model: 'Mustang', color: 'red' },
+        { brand: 'Honda', model: 'Civic', color: 'blue' },
+        { brand: 'Toyota', model: 'Camry', color: 'black' },
+        { brand: 'Tesla', model: 'Model 3', color: 'white' }
+
+
+    ];
     return (
         <>
-            <h1>Garage is used to store</h1>
+            <h1>Garage is used to store
+            <ul>
+                {CarList.map((car, index) => (
+                    <li key={index}>
+                        {car.brand} {car.model} {car.color}
+                    </li>
+                ))}     
+            </ul>
+            </h1>
             <Car />
         </>
     )
