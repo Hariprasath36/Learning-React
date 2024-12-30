84db5ea11bbd31963b3969af003fa918e8f77efe
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-12-30 09:34:56 +0530

    running 3:47:00

--- react-tutorial/src/components/Myform.js
+++ react-tutorial/src/components/Myform.js
@@ -1,10 +1,18 @@
 import React, {useState} from 'react'
 function Myform() {
     const[name,setName]=useState("");
+    // console.log('Current State',name);
+
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log('Form Submitted');
     console.log('Current State',name);
+    
+}
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>First Name : <input type="text" placeholder="First Name" onChange={(e)=>{setName(e.target.value)}} /></label>
+            <input type="submit" value="Submit" />
         </form>
     )
 }
