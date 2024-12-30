39202c0aa505cc21390d481c6cb29af181fa26c1
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-12-30 09:24:53 +0530

    running 3:41:00

--- react-tutorial/src/components/Myform.js
+++ react-tutorial/src/components/Myform.js
@@ -1,7 +1,10 @@
+import React, {useState} from 'react'
 function Myform() {
+    const[name,setName]=useState("");
+    console.log('Current State',name);
     return (
         <form>
-            <label>First Name : <input type="text" placeholder="First Name"/></label>
+            <label>First Name : <input type="text" placeholder="First Name" onChange={(e)=>{setName(e.target.value)}} /></label>
         </form>
     )
 }
