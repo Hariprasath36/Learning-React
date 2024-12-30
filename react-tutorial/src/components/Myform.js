8c0bd9dfa5b4230dd87af4f5659c4690d34d21a7
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-12-30 10:01:38 +0530

    running 4:00:00

--- react-tutorial/src/components/Myform.js
+++ react-tutorial/src/components/Myform.js
@@ -1,17 +1,22 @@
 import React, {useState} from 'react'
 function Myform() {
-    const[name,setName]=useState("");
+    // const[name,setName]=useState("");
+    // const[age,setAge]=useState("");
+    // const[email,setEmail]=useState("");
     // console.log('Current State',name);
+    const[input,setInput]=useState({});
 
 function handleSubmit(e) {
     e.preventDefault();
     console.log('Form Submitted');
-    console.log('Current State',name);
+    console.log('Current State',input);
     
 }
     return (
         <form onSubmit={handleSubmit}>
-            <label>First Name : <input type="text" placeholder="First Name" onChange={(e)=>{setName(e.target.value)}} /></label>
+            <label>First Name : <input type="text" placeholder="First Name" onChange={(e)=>{setInput((previousState) => {return {...previousState,firstName:e.target.value}})}} /></label><br/>
+            <label>Age : <input type="text" placeholder="age" onChange={(e)=>{setInput((previousState) => {return {...previousState,age:e.target.value}})}} /></label><br/>
+            <label>Email : <input type="text" placeholder="email" onChange={(e)=>{setInput((previousState) => {return {...previousState,email:e.target.value}})}} /></label>
             <input type="submit" value="Submit" />
         </form>
     )
