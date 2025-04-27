3ee21b8af87315a113dd1ad794a9517792f23f4c
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2025-04-27 23:00:40 +0530

    added

--- react-tutorial/src/components/Myform.js
+++ react-tutorial/src/components/Myform.js
@@ -22,27 +22,4 @@ function handleChange(e) {
             [name]: value}
     })
 }
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>First Name : <input type="text" name="firstName" placeholder="First Name" onChange={handleChange} /></label><br/>
-            <label>Age : <input type="text" name='age' placeholder="age" onChange={handleChange} /></label><br/>
-            <label>Number : <input type="text" name='num' placeholder="number" onChange={handleChange} value={input.num} /></label><br/>
-            <label>Email : <input type="text"  name='email' placeholder="email" onChange={handleChange} 
-            value={input.email}/></label><br/>
-
-            <select name="Country" onChange={handleChange} values={input.Country}>
-                <option>Select Country</option>
-                <option value="India">India</option>
-                <option value="USA">USA</option>
-                <option value="UK">UK</option>
-                <option value="AUS">AUS</option>
-                <option value="NZ">NZ</option>
-                <option value="SA">SA</option>
-                </select>
-            <br/>
-            <input type="submit" value="Submit" />
-        </form>
-    )
-}
-
-export default Myform;
+   
